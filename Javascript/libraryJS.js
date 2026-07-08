de5bc4c846dd8fdb96d421f1faf877a787d2d073
hello <23514@apps.eggs.school.nz>
2026-07-08 14:40:12 +1200

    Library Page - WIP: Book search

---
+++ Javascript/libraryJS.js
@@ -0,0 +1,3 @@
+// https://openlibrary.org/search.json?q=${encodeURIComponent(query)} 
+// https://covers.openlibrary.org/b/id/${boo.cover_i}-M.jpg
+// https://via.placeholder.com/150x200?text=No+Image
